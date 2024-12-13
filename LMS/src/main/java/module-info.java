f696a7183bc8f81715ecb77d8880c526b253c1f1
Nguyen Dinh Nhat Tan <tannguyendinh7@gmail.com>
2024-12-13 21:37:26 +0700

    UP

--- LMS/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.lms {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens com.example.lms to javafx.fxml;
-    exports com.example.lms;
-}
--- untitled9/src/Student.java
+++
@@ -1,5 +0,0 @@
-public class Student extends Person{
-    private String id;
-    public Person(int age,String name,String id){}
-
-}
